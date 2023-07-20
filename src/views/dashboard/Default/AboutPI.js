50e7a6e5acc7decfb988afafca5a669f3b7ab2f1
pramodrajarams <45769062+pramodrajarams@users.noreply.github.com>
2023-07-20 09:13:40 +0530

    cc

--- src/views/dashboard/Default/AboutPI.js
+++ src/views/dashboard/Default/AboutPI.js
@@ -8,9 +8,10 @@ const AboutSection = () => {
       <Card className="about-card">
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6} className="about-content">
-            <Typography variant="h1" style={{ fontSize: '46px', color: '#990000', display: 'flex', justifyContent: 'center' }}>
+            <Typography variant="h1" style={{ fontSize: '35px', color: '#800000', display: 'flex', justifyContent: 'center' }}>
               About PI
             </Typography>
+            <br/> 
             <Typography variant="body2" style={{ fontSize: '17px' }}>
               We are a team of passionate scientists dedicated to advancing knowledge and pushing the boundaries of scientific discovery.
               Our mission is to make groundbreaking contributions in various fields and improve the world we live in.We are a team of
