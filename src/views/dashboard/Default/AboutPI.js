95fe9ef0c62b640ce895f269e4224425b9254b59
pramodrajarams <pramodrajarams@gmail.com>
2023-07-27 21:51:17 +0530

    cc

--- src/views/dashboard/Default/AboutPI.js
+++ src/views/dashboard/Default/AboutPI.js
@@ -7,23 +7,13 @@ const AboutSection = () => {
     <Card style={{padding: '70px',  boxShadow: "3px 6px 9px 1px rgba(0, 0, 0, 0.2)"}}><h1 style={{ fontSize: '30px', color: '#990000',textAlign: 'center'}}>About PI</h1>
     <div  className="about-card">
       <Grid container spacing={2}>
-<<<<<<< HEAD
-        <Grid item xs={0} sm={0} md={6} style={{ margin: 'auto' }} className="hide-mobile">
-          <h1 style={{ fontSize: '30px', color: '#800000', margin: 'auto', textAlign: 'center' }}>About PI</h1>
-=======
-      
-        <Grid item xs={0} sm={0} md={5} style={{ margin: 'auto', marginTop:'0px' }} className="hide-mobile">
->>>>>>> 8fe0211dbb6df8ee088d411ab20bcf74bc250ef2
+         <Grid item xs={0} sm={0} md={5} style={{ margin: 'auto', marginTop:'0px' }} className="hide-mobile">
           <Avatar alt="PI" src={prs} className="user-avatar" style={{ marginTop: '15px' }} />
         </Grid>
         <Grid item xs={12} sm={12} md={7} className="about-content">
-          <Grid item xs={12} sm={12} md={0} style={{ margin: 'auto' }} className="hide-web">
-<<<<<<< HEAD
-            <h1 style={{ fontSize: '30px', color: '#800000', margin: 'auto', textAlign: 'center' }}>About PI</h1>
-=======
->>>>>>> 8fe0211dbb6df8ee088d411ab20bcf74bc250ef2
-            <br />
-            <Avatar alt="PI" src={prs} className="user-avatar" mt={1} />
+        <Grid item xs={12} sm={12} md={0} style={{ margin: 'auto' }} className="hide-web">
+        <br />
+          <Avatar alt="PI" src={prs} className="user-avatar" mt={1} />
           </Grid>
           <Typography variant="body2" style={{ fontSize: '17px', textAlign: 'justify' }}>
             <br />
@@ -33,33 +23,33 @@ const AboutSection = () => {
             <b style={{ color: '#800000' }}>Experience</b>
             <br />
             <br />
-            PI and Assistant Professor, Dept. of Systems and Computational Biology, University of Hyderabad, (Aug 2020 onwards)  <br />
-            Sr. Scientist, Systems Biology and Metabolic Engg. Lab, Dept. of Chemical Engg., IIT Bombay, INDIA (April 2019- May 2020)<br />
-            Harvard Associate, Bioengineering Div., Harvard School of Engineering and Applied Sciences, USA (April 2019-Mar 2021)  <br />                             
-            Research Associate, Bioengineering Div., Harvard School of Engineering and Applied Sciences, USA (Aug 2018-Mar 2019)<br />
-            Post-Doctoral Fellow, Bioengineering Div., Harvard  School of Engineering and Applied Sciences,  USA (Aug 2015-Mar 2019)<br />
-            Sr. Research Scientist, Biosystems Engg. Lab, Dept. of Chemical Engg., IIT Bombay, INDIA (Jan 2015-July 2015)<br />
-            Research Associate, Biosystems Engg. Lab, Dept. of Chemical Engg., IIT Bombay, INDIA (June 2014-Dec 2014)<br />
-            Technologist (R&D), Bio-Process Development, DSM Chemical Division, New Delhi, INDIA (Nov 2006- June 2009)<br />
-            Project Scholar, Dept. Fermentation Tech. and Bio-Engineering, CFTRI Mysore, INDIA (May 2005-Oct 2006)<br />
+            <b>PI and Assistant Professor </b>, Dept. of Systems and Computational Biology, University of Hyderabad, (Aug 2020 onwards)  <br />
+            <b>Sr. Scientist</b>, Systems Biology and Metabolic Engg. Lab, Dept. of Chemical Engg., IIT Bombay, INDIA (April 2019- May 2020)<br />
+            <b>Harvard Associate</b>Harvard Associate, Bioengineering Div., Harvard School of Engineering and Applied Sciences, USA (April 2019-Mar 2021)  <br />                             
+            <b>Research Associate</b>, Bioengineering Div., Harvard School of Engineering and Applied Sciences, USA (Aug 2018-Mar 2019)<br />
+            <b>Post-Doctoral Fellow</b>Post-Doctoral Fellow, Bioengineering Div., Harvard  School of Engineering and Applied Sciences,  USA (Aug 2015-Mar 2019)<br />
+            <b>Sr. Research Scientist</b>Sr. Research Scientist, Biosystems Engg. Lab, Dept. of Chemical Engg., IIT Bombay, INDIA (Jan 2015-July 2015)<br />
+            <b>Research Associate</b>Research Associate, Biosystems Engg. Lab, Dept. of Chemical Engg., IIT Bombay, INDIA (June 2014-Dec 2014)<br />
+            <b>Technologist (R&D)</b>Technologist (R&D), Bio-Process Development, DSM Chemical Division, New Delhi, INDIA (Nov 2006- June 2009)<br />
+            <b>Project Scholar</b>Project Scholar, Dept. Fermentation Tech. and Bio-Engineering, CFTRI Mysore, INDIA (May 2005-Oct 2006)<br />
             <br />
             <b style={{ color: '#800000' }}>Trainings</b>
             <br />
             <br />
-            Systems Biology and Chronotherapeutics, Harvard School of Engineering and Applied Sciences, Harvard University, USA.<br />
-            Biomedical Control Engineering, Harvard School of Engineering and Applied Sciences, Harvard University, USA.<br />
-            Network Medicine, Chrono-medicine, Sleep medicine and Lifestyle Medicine, Harvard Medical School, USA.<br />
-            AI in Healthcare and Medicine, Stanford University School of Medicine, Stanford University, USA.<br />
-            Biomedical Data Science, Harvard School of Public Health, Harvard University, USA.<br />
-            Principal and Practice of Clinical Research, National Institute of Health-NIH, USA.<br />
-            Epidemiological Research in Public Health, Imperial College of London, UK. <br />
+            <b>Systems Biology and Chronotherapeutics</b>, Harvard School of Engineering and Applied Sciences, Harvard University, USA.<br />
+            <b>Biomedical Control Engineering</b>, Harvard School of Engineering and Applied Sciences, Harvard University, USA.<br />
+            <b>Network Medicine, Chrono-medicine, Sleep medicine </b>and Lifestyle Medicine, Harvard Medical School, USA.<br />
+            <b>AI in Healthcare and Medicine</b>, Stanford University School of Medicine, Stanford University, USA.<br />
+            <b>Biomedical Data Science</b>, Harvard School of Public Health, Harvard University, USA.<br />
+            <b>Principal and Practice of Clinical Research</b>, National Institute of Health-NIH, USA.<br />
+            <b>Epidemiological Research in Public Health</b>, Imperial College of London, UK. <br />
             <br />
             <br />
             <b style={{ color: '#800000' }}>Memberships</b>
             <br />
             <br />
-            FRSPH: Fellow of The Royal Society for Public Health (UK)<br />
-            FISC:  Fellow of The Indian Society for Chronomedicine<br />
+            <b>FRSPH</b>: Fellow of The Royal Society for Public Health (UK)<br />
+            <b>FISC</b>:  Fellow of The Indian Society for Chronomedicine<br />
             Member of The Physiological Society (UK)       <br />
             Member of the European Society for Preventive Medicine (UK)  <br />
             Life Member of The Indian Society for Chronomedicine (ISCM)<br />
@@ -69,10 +59,10 @@ const AboutSection = () => {
             <b style={{ color: '#800000' }}>Education</b>
             <br />
             <br />
-            Ph.D, Biosystems Engineering, Dept. of Chemical Engg., IIT Bombay, INDIA, 2014.<br />
-            M.B.A, Professional with Operations and Projects Management, NIBM, INDIA, 2009.<br />
-            M.Tech, Chemical Technology (Food & Bioprocess Engg.), LIT Nagpur, INDIA, 2006.<br />
-            B.Tech, Chemical Technology, UDCT Aurangabad, INDIA, 2004.
+            <b>Ph.D</b>, Biosystems Engineering, Dept. of Chemical Engg., IIT Bombay, INDIA, 2014.<br />
+            <b>M.B.A</b>, Professional with Operations and Projects Management, NIBM, INDIA, 2009.<br />
+            <b>M.Tech</b>, Chemical Technology (Food & Bioprocess Engg.), LIT Nagpur, INDIA, 2006.<br />
+            <b>B.Tech</b>, Chemical Technology, UDCT Aurangabad, INDIA, 2004.
             </Typography>
         </Grid>
       </Grid></div>
