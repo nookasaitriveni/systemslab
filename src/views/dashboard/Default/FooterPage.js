d0252102bc764ee2aae4e03ae477f068fe1f2ee3
pramodrajarams <pramodrajarams@gmail.com>
2023-07-29 12:21:44 +0530

    cc

--- src/views/dashboard/Default/FooterPage.js
+++ src/views/dashboard/Default/FooterPage.js
@@ -76,23 +76,23 @@ const FooterPage = () => {
         </Grid>
         <Grid item xs={12} md={0} lg={0} className="hide-web">
           <Link component={RouterLink} to="/uoh" underline="none" sx={{ mx: 1 }}>
-            <img src={UOH} alt="UOH" style={{ height: '6.0vw' }} />
+            <img src={UOH} alt="UOH" style={{ height: '7vw' }} />
           </Link>
           {/*<Link component={RouterLink} to="/ioe" underline="none" sx={{ mx: 1 }}>
             <img src={DEP} alt="DEP" style={{ height: '6.5vw' }} />
           </Link>*/}
           <Link component={RouterLink} to="/ioe" underline="none" sx={{ mx: 1 }}>
-            <img src={DEP} alt="DEP" style={{ height: '6.0vw' }} />
+            <img src={DEP} alt="DEP" style={{ height: '7vw' }} />
           </Link>
           <Link component={RouterLink} to="/ioe" underline="none" sx={{ mx: 1 }}>
-            <img src={MOE} alt="MOE" style={{ height: '6.0vw' }} />
+            <img src={MOE} alt="MOE" style={{ height: '7vw' }} />
           </Link>
         </Grid>
         <Grid item xs={0} md={4} lg={4} className="right-footer hide-mobile">
           <Link component={RouterLink} to="/ioe" underline="none" sx={{ mx: 1 }}>
-            <img src={DEP} alt="DEP" style={{ height: '6.0vw' }} />        </Link>
+            <img src={DEP} alt="DEP" style={{ height: '6.25vw' }} />        </Link>
           <Link component={RouterLink} to="/ioe" underline="none" sx={{ mx: 1 }}>
-            <img src={MOE} alt="MOE" style={{ height: '6.0vw' }} />
+            <img src={MOE} alt="MOE" style={{ height: '6.25vw' }} />
           </Link>
         </Grid>
       </Grid>
