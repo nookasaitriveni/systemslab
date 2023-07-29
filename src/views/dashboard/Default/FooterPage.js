3e343f89ff4aa97570cecdcbe80335847050f796
pramodrajarams <pramodrajarams@gmail.com>
2023-07-29 13:18:14 +0530

    cc

--- src/views/dashboard/Default/FooterPage.js
+++ src/views/dashboard/Default/FooterPage.js
@@ -33,10 +33,10 @@ const FooterPage = () => {
       <Grid container spacing={2}>
         <Grid item xs={0} md={4} lg={4} className="left-footer hide-mobile">
           <Link component={RouterLink} to="/uoh" underline="none" sx={{ mx: 1 }}>
-            <img src={UOH} alt="UOH" style={{ height: '6.5vw' }} />
+            <img src={UOH} alt="UOH" style={{ height: '6.25vw' }} />
           </Link>
           <Link component={RouterLink} to="/ioe" underline="none" sx={{ mx: 1 }}>
-            <img src={IOE} alt="IOE" style={{ height: '6.5vw' }} />
+            <img src={IOE} alt="IOE" style={{ height: '6.25vw' }} />
             </Link>
         </Grid>
         <Grid item xs={12} md={4} lg={4} className="hide-mobile">
@@ -76,23 +76,23 @@ const FooterPage = () => {
         </Grid>
         <Grid item xs={12} md={0} lg={0} className="hide-web">
           <Link component={RouterLink} to="/uoh" underline="none" sx={{ mx: 1 }}>
-            <img src={UOH} alt="UOH" style={{ height: '6.5vw' }} />
+            <img src={UOH} alt="UOH" style={{ height: '6.25vw' }} />
           </Link>
           {/*<Link component={RouterLink} to="/ioe" underline="none" sx={{ mx: 1 }}>
             <img src={DEP} alt="DEP" style={{ height: '6.5vw' }} />
           </Link>*/}
           <Link component={RouterLink} to="/ioe" underline="none" sx={{ mx: 1 }}>
-            <img src={DEP} alt="DEP" style={{ height: '6.5vw' }} />
+            <img src={DEP} alt="DEP" style={{ height: '6.25vw' }} />
           </Link>
           <Link component={RouterLink} to="/ioe" underline="none" sx={{ mx: 1 }}>
-            <img src={MOE} alt="MOE" style={{ height: '6.5vw' }} />
+            <img src={MOE} alt="MOE" style={{ height: '6.25vw' }} />
           </Link>
         </Grid>
         <Grid item xs={0} md={4} lg={4} className="right-footer hide-mobile">
           <Link component={RouterLink} to="/ioe" underline="none" sx={{ mx: 1 }}>
-            <img src={DEP} alt="DEP" style={{ height: '6.5vw' }} />        </Link>
+            <img src={DEP} alt="DEP" style={{ height: '6.25vw' }} />        </Link>
           <Link component={RouterLink} to="/ioe" underline="none" sx={{ mx: 1 }}>
-            <img src={MOE} alt="MOE" style={{ height: '6.5vw' }} />
+            <img src={MOE} alt="MOE" style={{ height: '6.25vw' }} />
           </Link>
         </Grid>
       </Grid>
