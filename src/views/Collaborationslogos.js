cb39168c71d6e3d4e382c79a6035c6d5040d68b8
pramodrajarams <pramodrajarams@gmail.com>
2023-12-01 15:45:02 +0530

    cc

--- src/views/Collaborationslogos.js
+++ src/views/Collaborationslogos.js
@@ -6,11 +6,13 @@ import Apolo_IMSR2 from './Collaborations_logos/Apolo_IMSR-logo-rev2.png';
 import Asian_health_care_foundation from './Collaborations_logos/Asian_health_care_foundation.png';
 import EMORY_logo from './Collaborations_logos/Emory_GDRC_logo.jpg';
 import harvard_school_engineering_and_applied_sciences_logo from './Collaborations_logos/SEAS_Logo.webp';
+import Harvard_logo from './Collaborations_logos/SEAS_Logo.webp';
 import HMS1 from './Collaborations_logos/HMS1.png';
 import IIPH from './Collaborations_logos/IIPH_Hyderabad.jpg';
 
 import IIT_Hyderabad_Insignia from './Collaborations_logos/IIT_Hyderabad_Insignia.svg.png';
-import Indian_Institute_of_Technology_Bombay_Logo from './Collaborations_logos/Indian_Institute_of_Technology_Bombay_Logo.svg.png';
+import Indian_Institute_of_Technology_Bombay_Logo from './Collaborations_logos/IITB_logo1.png';
+import IITD_Logo from './Collaborations_logos/IITD_logo1.jpg';
 import ISB_logo_lt from './Collaborations_logos/ISB_logo_lt-1.png';
 import logo_UCSF_signature_0 from './Collaborations_logos/UCSF_logo.jpg';
 import Johns_Medical_College_logo from './Collaborations_logos/St._Johns_Medical_College_logo.png';
@@ -30,8 +32,8 @@ export default class Collaborationslogos extends Component {
          <div className="logo-container">
           
         <div>
-          <img src={harvard_school_engineering_and_applied_sciences_logo} alt="Logo 1" className="logo" />
-          <img src={HMS1} alt="Logo 1" className="logo" />
+          <img src={Harvard_logo} alt="Logo 1" className="logo" />
+          <img src={HMS1} alt="Logo 1" className="logo" /> 
           <img src={logo_UCSF_signature_0} alt="Logo 1" className="logo" />
           <img src={ISB_logo_lt} alt="Logo 1" className="logo" />
           <img src={Phenome} alt="Logo 1" className="logo" />
@@ -42,6 +44,7 @@ export default class Collaborationslogos extends Component {
             <img src={Johns_Medical_College_logo} alt="Logo 1" className="logo" />
             <img src={Apolo_IMSR} alt="Logo 2" className="logo" />
             <img src={Indian_Institute_of_Technology_Bombay_Logo} alt="Logo 1" className="logo" />
+            <img src={IITD_Logo} alt="Logo 1" className="logo" />
             <img src={IIPH} alt="Logo 1" className="logo" />
             {/* <img src={Apolo_IMSR2} alt="Logo 3" className="logo" /> */}
             {/* <img src={Asian_health_care_foundation} alt="Logo 1" className="logo" /> */}
