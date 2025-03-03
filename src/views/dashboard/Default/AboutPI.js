b85dfc129e50d02a8545c6532fa63a3996138486
pramodrajarams <pramodrajarams@gmail.com>
2025-03-03 17:53:36 +0530

    cc

--- src/views/dashboard/Default/AboutPI.js
+++ src/views/dashboard/Default/AboutPI.js
@@ -25,7 +25,7 @@ const AboutSection = () => {
             <br />
             <br />
             <b>PI and Asst. Professor</b>, Dept. of Systems and Computational Biology, University of Hyderabad, India (Aug 2020 onwards) <br />
-            <b>Faculty & CoPI </b>, Centre for Translational Research on Endocrine Pancreas & Diabetes, AHF-AIG Hospitals, Inida (Mar 2024 onwards) <br /> 
+            <b>Faculty & CoPI</b>, Centre for Translational Research on Endocrine Pancreas & Diabetes, AHF-AIG Hospitals, Inida (Mar 2024 onwards) <br /> 
             <b>Sr. Scientist</b>, Systems Biology and Metabolic Engg. Lab, Dept. of Chemical Engg., IIT Bombay, India (April 2019- May 2020)<br />
             <b>Harvard Associate</b>, Bioengineering Div., Harvard School of Engineering and Applied Sciences, USA (April 2019-Mar 2021)  <br />
             <b>Research Associate</b>, Bioengineering Div., Harvard School of Engineering and Applied Sciences, USA (Aug 2018-Mar 2019)<br />
